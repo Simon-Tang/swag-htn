d7d5f05ad81c76c837acd3284a933ac2a73f398a
Jasvin Baweja <gurtejsuri@gmail.com>
2017-09-16 15:22:27 -0400

    sign in page

---
+++ swag-htn/swag-htn/LoginViewController.swift
@@ -0,0 +1,94 @@
+//
+//  LoginViewController.swift
+//  swag-htn
+//
+//  Created by Jasvin Baweja on 2017-09-16.
+//
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var textFieldLoginEmail: UITextField!
+    @IBOutlet weak var textFieldLoginPassword: UITextField!
+    @IBOutlet weak var btnSignIn: UIButton!
+    @IBOutlet weak var btnSignUp: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signUpDidTouch(_ sender: Any) {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Register",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) { action in
+                                        // 1
+                                        let emailField = alert.textFields![0]
+                                        let passwordField = alert.textFields![1]
+                                        
+                                        // 2
+                                        Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { user, error in
+                                            if error == nil {
+                                                // 3
+                                                Auth.auth().signIn(withEmail: self.textFieldLoginEmail.text!, password: self.textFieldLoginPassword.text!)
+                                                print("GET HERE? PASSSS")
+                                                self.performSegue(withIdentifier: "signedIn", sender: nil)
+                                            } else {
+                                                print("ERRORRRRR")
+                                            }
+                                        }
+                                        
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email"
+        }
+        
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func signInDidTouch(_ sender: Any) {
+        Auth.auth().signIn(withEmail: textFieldLoginEmail.text!, password: textFieldLoginPassword.text!) { (user, error) in
+            if (error == nil) {
+                self.performSegue(withIdentifier: "signedIn", sender: nil)
+            } else {
+                print("ERRORRRRR2")
+            }
+        }
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
